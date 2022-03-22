e762651226c1a0b8fd09bc36bcdcd18dd135a35c
khairul-abdi <kabdi384@gmail.com>
2022-03-22 08:42:30 +0700

    update

--- controllers/weather.controller.go
+++ controllers/weather.controller.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"assignment_3/models"
 	"fmt"
+	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -13,20 +15,22 @@ var result models.Weather
 func Get(c *gin.Context) {
 	result = models.GetStatus()
 
-	go Looper()
-
+	go Looper(c)
+	fmt.Println("First => ", result)
 	c.JSON(200, gin.H{
 		"message": result,
 	})
 }
 
-func Looper() {
+func Looper(c *gin.Context) {
 
 	ch := time.Tick(15 * time.Second)
 	for next := range ch {
-		fmt.Println(next)
 		result = models.GetStatus()
 
-		fmt.Println(result)
+		fmt.Println("NEXT -> ", next)
+		fmt.Println("HASIL NEXT => ", result)
+		location := url.URL{Path: "/"}
+		c.Redirect(http.StatusFound, location.RequestURI())
 	}
 }
